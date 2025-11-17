ca19dfccb496f808a4474de58922d85245f8d118
DanielBelow <daniel.below@tum.de>
2025-11-17 23:31:51 +0100

    initial commit

---
+++ src/day01.rs
@@ -0,0 +1,25 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day01)]
+pub fn generate(_: &str) -> Option<Vec<u64>> {
+    todo!()
+}
+
+#[aoc(day01, part1)]
+pub fn part1(_: &[u64]) -> usize {
+    todo!()
+}
+
+#[aoc(day01, part2)]
+pub fn part2(_: &[u64]) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_p1() {}
+
+    #[test]
+    fn test_p2() {}
+}
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![deny(unsafe_code)]
+#![warn(nonstandard_style, rust_2018_idioms)]
+#![allow(
+    clippy::implicit_hasher,
+    clippy::similar_names,
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss
+)]
+
+mod day01;
+
+aoc_runner_derive::aoc_lib! { year = 2025 }
